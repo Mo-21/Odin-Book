f9bd36e3e0c918fe7cf12b46100fd6e157a7256b
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-28 11:57:14 +0300

    Add Custom Hook To Fetch Posts From User & Followers

--- client/src/components/Feed/Feed.tsx
+++ client/src/components/Feed/Feed.tsx
@@ -1,11 +1,22 @@
 import Post from "../PostInput/Post";
 import Timeline from "../Timeline/Timeline";
+import useTimeline from "../Timeline/useTimeline";
 
 export default function Feed() {
+  const { timeline, loading, error } = useTimeline();
+
   return (
     <div className="feed">
       <Post />
-      <Timeline />
+      {loading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {!loading && !error && timeline.length === 0 && <div>No posts</div>}
+      {!loading &&
+        !error &&
+        timeline.length > 0 &&
+        timeline.map((post) => {
+          return <Timeline key={post._id} post={post} />;
+        })}
     </div>
   );
 }
