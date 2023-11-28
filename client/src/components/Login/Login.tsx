d7d88cc7b8f1574acea08225a4717d1731a8af36
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-28 04:14:04 +0300

    Add Custom Hook To Fetch Data Using RQ

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,7 +1,20 @@
 import "./Login.css";
 import logo from "../../assets/link.svg";
+import { useState } from "react";
+import useLogin from "./useLogin";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const login = useLogin(() => {
+    setEmail("");
+    setPassword("");
+  });
+
   return (
     <div className="login-container">
       <div className="logo">
@@ -11,10 +24,33 @@ export default function Login() {
         </span>
         <img src={logo} alt="logo" />
       </div>
-      <form className="login-form">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          login.mutate({ email, password });
+          navigate("/");
+        }}
+        className="login-form"
+      >
         <div className="login-input">
-          <input placeholder="Email" className="input" />
-          <input placeholder="Password" className="input" />
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="Email"
+            className="input"
+            type="text"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="Password"
+            className="input"
+          />
         </div>
         <div className="login-btn">
           <button type="submit" className="btn-login">
---
+++ client/src/components/Login/useLogin.ts
@@ -0,0 +1,44 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import ClientAPI from "../ClientAPI";
+import { useNavigate } from "react-router-dom";
+
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  _id: string;
+  username: string;
+  email: string;
+  isAdmin: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface AddUserContext {
+  previousUser: LoginResponse;
+}
+
+function useLogin(onLogin: () => void) {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const Client = new ClientAPI<LoginRequest, LoginResponse>("/auth/login");
+
+  return useMutation<LoginResponse, Error, LoginRequest, AddUserContext>({
+    mutationFn: Client.login,
+    onSuccess: (response) => {
+      const previousUser = queryClient.setQueryData<LoginResponse>(
+        ["user"],
+        response
+      );
+      console.log(response);
+      navigate(`/${response._id}`);
+      onLogin();
+      return previousUser;
+    },
+  });
+}
+
+export default useLogin;
