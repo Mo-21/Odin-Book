8b1b3470cf4626aff3d2363f3902280984199153
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-29 02:44:18 +0300

    Refactor like and dislike functionality in post route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -59,15 +59,15 @@ router.delete(
 router.put("/:id/like", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if (post.likes.includes(req.body.userId)) {
-      await Post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(200).json("The post has been disliked!");
-    } else {
+    if (!post.likes.includes(req.body.userId)) {
       await post.updateOne({ $push: { likes: req.body.userId } });
-      res.status(200).json("The post has been liked!");
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
     }
   } catch (err) {
-    res.status(500).json(err.message);
+    res.status(500).json(err);
   }
 });
 
