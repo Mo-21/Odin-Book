4a2c9c2513d7422bd580352ca83cbbb6ce01cc86
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-28 14:25:09 +0300

    Add get all posts route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,19 +7,15 @@ const Post = require("../models/Post");
 const { User } = require("../models/User");
 
 // Create a Post
-router.post(
-  "/",
-  [authorize, passport.authenticate("jwt", { session: false })],
-  async (req, res) => {
-    try {
-      const newPost = new Post(req.body);
-      const savedPost = await newPost.save();
-      res.status(200).json(savedPost);
-    } catch (err) {
-      res.status(500).json(err.message);
-    }
+router.post("/", async (req, res) => {
+  try {
+    const newPost = new Post(req.body);
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err.message);
   }
-);
+});
 
 // Edit a Post
 router.put(
@@ -60,57 +56,56 @@ router.delete(
 );
 
 // Like/Dislike a Post
-router.put(
-  "/:id/like",
-  [authorize, passport.authenticate("jwt", { session: false })],
-  async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      if (post.likes.includes(req.body.userId)) {
-        await Post.updateOne({ $pull: { likes: req.body.userId } });
-        res.status(200).json("The post has been disliked!");
-      } else {
-        await post.updateOne({ $push: { likes: req.body.userId } });
-        res.status(200).json("The post has been liked!");
-      }
-    } catch (err) {
-      res.status(500).json(err.message);
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.likes.includes(req.body.userId)) {
+      await Post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked!");
+    } else {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked!");
     }
+  } catch (err) {
+    res.status(500).json(err.message);
   }
-);
+});
 
 // Get a Post
-router.get(
-  "/:id",
-  [authorize, passport.authenticate("jwt", { session: false })],
-  async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      res.status(200).json(post);
-    } catch (err) {
-      res.status(500).json(err.message);
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err.message);
   }
-);
+});
+
+//Get Users Posts
+router.get("/profile/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const posts = await Post.find({ userId: user._id });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
 // Get Timeline Posts
-router.get(
-  "/timeline/:id",
-  [authorize, passport.authenticate("jwt", { session: false })],
-  async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-      const postsFromUser = await Post.find({ userId: user._id });
-      const postsFromFollowers = await Promise.all(
-        user.followings.map((followingId) => {
-          return Post.find({ userId: followingId });
-        })
-      );
-      res.status(200).json(postsFromUser.concat(...postsFromFollowers));
-    } catch (err) {
-      res.status(500).json(err.message);
-    }
+router.get("/timeline/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const postsFromUser = await Post.find({ userId: user._id });
+    const postsFromFollowers = await Promise.all(
+      user.followings.map((followingId) => {
+        return Post.find({ userId: followingId });
+      })
+    );
+    res.status(200).json(postsFromUser.concat(...postsFromFollowers));
+  } catch (err) {
+    res.status(500).json(err.message);
   }
-);
+});
 
 module.exports = router;
