d0a91dd1b65b2a68cc38b4c81d8aa789000d8d90
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-28 11:57:41 +0300

    Refactor Timeline component to display user details and post content dynamically

--- client/src/components/Timeline/Timeline.tsx
+++ client/src/components/Timeline/Timeline.tsx
@@ -1,24 +1,33 @@
 import "./Timeline.css";
-import profilePicture from "../../assets/profile-picture.jpg";
 import welcome from "../../assets/welcome.jpg";
 import UnfoldMoreOutlinedIcon from "@mui/icons-material/UnfoldMoreOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import GradeOutlinedIcon from "@mui/icons-material/GradeOutlined";
+import { Post } from "./useTimeline";
+import useAuthorDetails from "./useAuthorDetails";
 
-export default function Timeline() {
+export default function Timeline({ post }: { post: Post }) {
+  const { user, loading, error } = useAuthorDetails({ userId: post.userId });
+
+  console.log(user);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
+  const PF = import.meta.env.VITE_PUBLIC_FOLDER;
+
   return (
     <div className="timeline">
       <div className="post-container">
         <div className="user-details">
           <div className="author-details">
-            <img src={profilePicture} alt="profilePicture" />
-            <span className="username">Mo-21</span>
+            <img src={PF + user.profilePicture} alt="profilePicture" />
+            <span className="username">{user.username}</span>
             <span className="post-date">5 mins ago</span>
           </div>
           <UnfoldMoreOutlinedIcon className="more-icon" />
         </div>
         <div className="body">
-          <div className="post-content">Welcome to my platform!</div>
+          <div className="post-content">{post.content}</div>
           <img src={welcome} alt="welcome" />
         </div>
         <div className="post-interaction">
@@ -33,43 +42,14 @@ export default function Timeline() {
               titleAccess="like"
               className="post-interaction-icon"
             />
-            <span className="like-counter">23 people like this</span>
+            <span className="like-counter">
+              {post.likes.length ? post.likes.length : 0} people like this
+            </span>
           </div>
           <div className="comments">
-            <span className="comment-counter">43 comments</span>
-            <hr />
-          </div>
-        </div>
-      </div>
-      <div className="post-container">
-        <div className="user-details">
-          <div className="author-details">
-            <img src={profilePicture} alt="profilePicture" />
-            <span className="username">Mo-21</span>
-            <span className="post-date">5 mins ago</span>
-          </div>
-          <UnfoldMoreOutlinedIcon className="more-icon" />
-        </div>
-        <div className="body">
-          <div className="post-content">Welcome to my platform!</div>
-          <img src={welcome} alt="welcome" />
-        </div>
-        <div className="post-interaction">
-          <div className="rate">
-            <GradeOutlinedIcon
-              titleAccess="star"
-              className="post-interaction-icon"
-            />
-          </div>
-          <div className="like">
-            <FavoriteBorderOutlinedIcon
-              titleAccess="like"
-              className="post-interaction-icon"
-            />
-            <span className="like-counter">23 people like this</span>
-          </div>
-          <div className="comments">
-            <span className="comment-counter">43 comments</span>
+            <span className="comment-counter">
+              {post.comments.toString() ? post.comments.toString() : 0} comments
+            </span>
             <hr />
           </div>
         </div>
