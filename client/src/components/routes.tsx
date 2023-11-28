1f0c32941fe880581117c18909e26d68590a7eaa
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-28 14:24:17 +0300

    Refactor routes component

--- client/src/components/routes.tsx
+++ client/src/components/routes.tsx
@@ -3,22 +3,15 @@ import Home from "./pages/Home";
 import Profile from "./Profile/Profile";
 import Login from "./Login/Login";
 import Register from "./Register/Register";
-import App from "./pages/App";
 
 const router = createBrowserRouter([
   {
+    element: <Home />,
     path: "/",
-    element: <App />,
-    children: [
-      {
-        path: ":id",
-        element: <Home />,
-      },
-      {
-        path: "profile/:id",
-        element: <Profile />,
-      },
-    ],
+  },
+  {
+    path: "profile/:id",
+    element: <Profile />,
   },
   {
     path: "/login",
