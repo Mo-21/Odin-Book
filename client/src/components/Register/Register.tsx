1835d5259bdc923b33e85c277acc502981c9fc33
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-28 22:17:00 +0300

    Create Registration Component with Custom Hook To Mutate Data

--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,7 +1,15 @@
 import "../Login/Login.css";
 import logo from "../../assets/link.svg";
+import { useRef } from "react";
+import useRegister from "./useRegister";
 
 export default function Register() {
+  const email = useRef<HTMLInputElement>(null);
+  const password = useRef<HTMLInputElement>(null);
+  const username = useRef<HTMLInputElement>(null);
+  const passwordConfirmation = useRef<HTMLInputElement>(null);
+
+  const register = useRegister(() => {});
   return (
     <div className="login-container">
       <div className="logo">
@@ -12,12 +20,55 @@ export default function Register() {
 
         <img src={logo} alt="logo" />
       </div>
-      <form className="login-form">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (
+            email.current?.value &&
+            password.current?.value &&
+            username.current?.value &&
+            passwordConfirmation.current?.value
+          )
+            register.mutate({
+              email: email.current?.value,
+              password: password.current?.value,
+              username: username.current?.value,
+              passwordConfirmation: passwordConfirmation.current?.value,
+            });
+        }}
+        className="login-form"
+      >
         <div className="login-input">
-          <input placeholder="Username" className="input" />
-          <input placeholder="Email" className="input" />
-          <input placeholder="Password" className="input" />
-          <input placeholder="Confirm Password" className="input" />
+          <input
+            ref={username}
+            required
+            type="text"
+            placeholder="Username"
+            className="input"
+          />
+          <input
+            ref={email}
+            required
+            type="email"
+            placeholder="Email"
+            className="input"
+          />
+          <input
+            ref={password}
+            required
+            type="password"
+            minLength={6}
+            placeholder="Password"
+            className="input"
+          />
+          <input
+            required
+            ref={passwordConfirmation}
+            type="password"
+            minLength={6}
+            placeholder="Confirm Password"
+            className="input"
+          />
         </div>
         <div className="login-btn">
           <button type="submit" className="btn-login">
---
+++ client/src/components/Register/useRegister.ts
@@ -0,0 +1,53 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import ClientAPI from "../ClientAPI";
+import { useNavigate } from "react-router-dom";
+
+interface RegistrationRequest {
+  email: string;
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+export interface RegistrationResponse {
+  _id: string;
+  username: string;
+  email: string;
+  isAdmin: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface AddUserContext {
+  previousUser: RegistrationResponse;
+}
+
+function useRegister(onRegister: () => void) {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const Client = new ClientAPI<RegistrationRequest, RegistrationResponse>(
+    "/auth/register"
+  );
+
+  return useMutation<
+    RegistrationResponse,
+    Error,
+    RegistrationRequest,
+    AddUserContext
+  >({
+    mutationFn: Client.login,
+    onSuccess: (response) => {
+      const previousUser = queryClient.setQueryData<RegistrationResponse>(
+        ["user"],
+        response
+      );
+      console.log(response);
+      navigate(`/${response._id}`);
+      onRegister();
+      return previousUser;
+    },
+  });
+}
+
+export default useRegister;
