bb3e040ae617b50509f8b75233283b13da2b1b10
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-28 04:14:34 +0300

    Add ClientAPI class for making API requests

---
+++ client/src/components/ClientAPI.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/api",
+});
+
+class ClientAPI<T, R> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  login = (data: T) => {
+    return axiosInstance.post<R>(this.endpoint, data).then((res) => res.data);
+  };
+}
+
+export default ClientAPI;
