1fbb657b681de48e8e1f37babf8d8b1a1375488f
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-29 07:59:53 +0300

    Refactor PostInput component to include file upload and handle form submission

--- client/src/components/PostInput/Post.tsx
+++ client/src/components/PostInput/Post.tsx
@@ -1,25 +1,116 @@
 import "./Post.css";
-import profilePicture from "../../assets/profile-picture.jpg";
 import PermMediaOutlinedIcon from "@mui/icons-material/PermMediaOutlined";
 import TagOutlinedIcon from "@mui/icons-material/TagOutlined";
 import AlternateEmailOutlinedIcon from "@mui/icons-material/AlternateEmailOutlined";
 import AddLocationAltOutlinedIcon from "@mui/icons-material/AddLocationAltOutlined";
+import useAuth from "../Login/useAuth";
+import { useRef, useState } from "react";
+import axios from "axios";
+import { PostResponse } from "../Timeline/useTimeline";
+import ClientAPI from "../ClientAPI";
+import { Cancel } from "@mui/icons-material";
+
+interface NewPost {
+  userId: string;
+  content: string;
+  img?: string;
+}
 
 export default function Post() {
+  const status = useRef<HTMLInputElement>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const user = useAuth();
+
+  const PF = import.meta.env.VITE_PUBLIC_FOLDER;
+
+  const Client = new ClientAPI<NewPost, PostResponse>("/posts/");
+
+  const handleFormSubmission = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    setFile(null);
+    if (
+      !user ||
+      status.current?.value === null ||
+      status.current?.value === undefined
+    )
+      return console.log("User not logged in", user);
+    const newPost: NewPost = {
+      userId: user?._id,
+      content: status.current?.value,
+    };
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("file", file);
+      newPost.img = fileName;
+      console.log(newPost);
+      try {
+        await axios.post("/api/upload", data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    try {
+      const res = await Client.sharePost(newPost);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="post-input">
       <div className="share-input">
         <img
           className="share-profile-img"
-          src={profilePicture}
+          src={
+            PF +
+            (user?.profilePicture ? user.profilePicture : "users/default.png")
+          }
           alt="profilePicture"
         />
-        <input className="share-input-text" placeholder="Express yourself..." />
+        <input
+          ref={status}
+          className="share-input-text"
+          placeholder={`Express yourself ${user?.username}, don't be shy.`}
+        />
+        {file && (
+          <div className="share-img-container">
+            <img
+              className="share-img"
+              src={URL.createObjectURL(file)}
+              alt="post-image"
+            />
+            <Cancel
+              className="share-cancel-img"
+              onClick={() => setFile(null)}
+            />
+          </div>
+        )}
       </div>
-      <div className="post-actions">
+      <form onSubmit={handleFormSubmission} className="post-actions">
         <div className="media">
-          <PermMediaOutlinedIcon className="action-icon" />
-          <span className="media-text">Media</span>
+          <label htmlFor="file" className="media-text">
+            <PermMediaOutlinedIcon className="action-icon" />
+            Media
+            <input
+              type="file"
+              id="file"
+              style={{ display: "none" }}
+              accept=".png,.jpg,.jpeg,.svg"
+              onChange={(event) => {
+                if (
+                  event.target.files === null ||
+                  event.target.files[0] === null
+                )
+                  return;
+                setFile(event.target?.files[0]);
+              }}
+            />
+          </label>
         </div>
         <div className="hashtag">
           <TagOutlinedIcon className="action-icon" />
@@ -33,10 +124,10 @@ export default function Post() {
           <AddLocationAltOutlinedIcon className="action-icon" />
           <span className="location-text">Location</span>
         </div>
-      </div>
-      <div className="submit-btn">
-        <button className="btn">Submit</button>
-      </div>
+        <div className="submit-btn">
+          <button className="btn">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
