54163de9d7afe2baf4218ab354531a29eb8a542f
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-01 01:15:29 +0300

    Populate User model with each message response

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const Message = require("../models/Message");
+const { User } = require("../models/User");
 
 // send message
 router.post("/", async (req, res) => {
@@ -18,9 +19,15 @@ router.get("/:conversationId", async (req, res) => {
   try {
     const messages = await Message.find({
       conversationId: req.params.conversationId,
+    }).populate({
+      path: "sender",
+      model: User,
+      select: "username profilePicture _id",
     });
     res.status(200).json(messages);
   } catch (err) {
     res.status(500).json(err.message);
   }
 });
+
+module.exports = router;
