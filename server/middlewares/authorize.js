fdd9885c0a75d57383b59fa125ed6bdbf703feed
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-30 03:17:27 +0300

    Fix authorization middleware to handle undefined access token

--- server/middlewares/authorize.js
+++ server/middlewares/authorize.js
@@ -5,10 +5,13 @@ const authorize = async (req, res, next) => {
 
   if (req.cookies.accessToken === null) {
     return res.status(400).json({ message: "Invalid access token" });
+  } else if (req.cookies.accessToken === undefined) {
+    return res.status(401).json({ message: "undefined access token" });
   } else {
     try {
       //Get token from header
       token = req.cookies.accessToken;
+      console.log(token);
       const decoded = jwt.verify(token, process.env.JWT_ACCESS_KEY);
       if (!token) return res.status(401).json("Not Authorized!");
       //Get the user from the token
