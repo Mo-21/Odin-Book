009aa850a3bba443b0e6bc4ddc5cd511ae72889f
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-02 11:36:28 +0300

    Refactor API endpoints in ClientAPI class

--- client/src/components/ClientAPI.ts
+++ client/src/components/ClientAPI.ts
@@ -1,27 +1,7 @@
 import axios from "axios";
 
-const axiosInstanceLogin = axios.create({
-  baseURL: "/api/auth",
-});
-
-const axiosInstanceUsers = axios.create({
-  baseURL: "/api/users",
-});
-
-const axiosInstancePosts = axios.create({
-  baseURL: "/api/posts",
-});
-
-const axiosInstanceComments = axios.create({
-  baseURL: "/api/comments",
-});
-
-const axiosInstanceConversations = axios.create({
-  baseURL: "/api/conversations",
-});
-
-const axiosInstanceMessages = axios.create({
-  baseURL: "/api/messages",
+const axiosInstance = axios.create({
+  baseURL: "/api",
 });
 
 class ClientAPI<T, R> {
@@ -32,85 +12,75 @@ class ClientAPI<T, R> {
   }
 
   login = (data: T) => {
-    return axiosInstanceLogin
-      .post<R>(this.endpoint, data)
-      .then((res) => res.data);
+    return axiosInstance.post<R>(this.endpoint, data).then((res) => res.data);
   };
 
   getTimeline = () => {
-    return axiosInstancePosts.get<R>(this.endpoint).then((res) => res.data);
+    return axiosInstance.get<R>(this.endpoint).then((res) => res.data);
   };
 
   getUser = () => {
-    return axiosInstanceUsers.get<R>(this.endpoint).then((res) => res.data);
+    return axiosInstance.get<R>(this.endpoint).then((res) => res.data);
   };
 
   getAllUser = () => {
-    return axiosInstanceUsers.get<R>(this.endpoint).then((res) => res.data);
+    return axiosInstance.get<R>(this.endpoint).then((res) => res.data);
   };
 
   updateProfile = (data: T) => {
-    return axiosInstanceUsers.put(this.endpoint, data).then((res) => res.data);
+    return axiosInstance.put(this.endpoint, data).then((res) => res.data);
   };
 
   likePost = (data: T) => {
-    return axiosInstancePosts.put(this.endpoint, data).then((res) => res.data);
+    return axiosInstance.put(this.endpoint, data).then((res) => res.data);
   };
 
   sharePost = (data: T) => {
-    return axiosInstancePosts
-      .post<R>(this.endpoint, data)
-      .then((res) => res.data);
+    return axiosInstance.post<R>(this.endpoint, data).then((res) => res.data);
   };
 
   fetchFriends = () => {
-    return axiosInstanceUsers.get<R>(this.endpoint).then((res) => res.data);
+    return axiosInstance.get<R>(this.endpoint).then((res) => res.data);
   };
 
   followUser = (data: T) => {
-    return axiosInstanceUsers.put(this.endpoint, data).then((res) => res.data);
+    return axiosInstance.put(this.endpoint, data).then((res) => res.data);
   };
 
   unFollowUser = (data: T) => {
-    return axiosInstanceUsers.put(this.endpoint, data).then((res) => res.data);
+    return axiosInstance.put(this.endpoint, data).then((res) => res.data);
   };
 
   commentPost = (data: T) => {
-    return axiosInstanceComments
-      .post<R>(this.endpoint, data)
-      .then((res) => res.data);
+    return axiosInstance.post<R>(this.endpoint, data).then((res) => res.data);
   };
 
   deleteComment = (data: T) => {
-    return axiosInstanceComments
+    return axiosInstance
       .delete(this.endpoint, { data })
       .then((res) => res.data);
   };
 
   updatePost = (data: T) => {
-    return axiosInstancePosts.put(this.endpoint, data).then((res) => res.data);
+    return axiosInstance.put(this.endpoint, data).then((res) => res.data);
   };
 
   deletePost = (data: T) => {
-    return axiosInstancePosts
+    return axiosInstance
       .delete(this.endpoint, { data })
       .then((res) => res.data);
   };
 
   getConversation = () => {
-    return axiosInstanceConversations
-      .get<R>(this.endpoint)
-      .then((res) => res.data);
+    return axiosInstance.get<R>(this.endpoint).then((res) => res.data);
   };
 
   getMessages = () => {
-    return axiosInstanceMessages.get<R>(this.endpoint).then((res) => res.data);
+    return axiosInstance.get<R>(this.endpoint).then((res) => res.data);
   };
 
   sendMessage = (data: T) => {
-    return axiosInstanceMessages
-      .post<R>(this.endpoint, data)
-      .then((res) => res.data);
+    return axiosInstance.post<R>(this.endpoint, data).then((res) => res.data);
   };
 }
 
